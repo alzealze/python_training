982266d1a65621e1eebfb2029e3f5c1a3d3fceda
alzealze <Zemskovjob4@gmail.com>
2023-09-24 22:30:08 +0300

    сценарий почищен от неиспользуемых методов, удалены лишние импорты

--- test_add_contacts.py
+++ test_add_contacts.py
@@ -1,20 +1,14 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
-import unittest, time, re
+import unittest
+
 
 class TestAddContacts(unittest.TestCase):
     def setUp(self):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
-        self.base_url = "https://www.google.com/"
-        self.verificationErrors = []
-        self.accept_next_alert = True
-    
+
     def test_add_contacts(self):
         wd = self.wd
         wd.get("http://localhost/addressbook")
@@ -43,31 +37,10 @@ class TestAddContacts(unittest.TestCase):
         wd.find_element(By.XPATH, "//div[@id='content']/form/input[21]").click()
         wd.find_element(By.LINK_TEXT, "home page").click()
         wd.find_element(By.LINK_TEXT, "Logout").click()
-    
-    def is_element_present(self, how, what):
-        try: self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
-    
-    def is_alert_present(self):
-        try: self.wd.switch_to_alert()
-        except NoAlertPresentException as e: return False
-        return True
-    
-    def close_alert_and_get_its_text(self):
-        try:
-            alert = self.wd.switch_to_alert()
-            alert_text = alert.text
-            if self.accept_next_alert:
-                alert.accept()
-            else:
-                alert.dismiss()
-            return alert_text
-        finally: self.accept_next_alert = True
-    
+
     def tearDown(self):
         self.wd.quit()
-        self.assertEqual([], self.verificationErrors)
+
 
 if __name__ == "__main__":
     unittest.main()
