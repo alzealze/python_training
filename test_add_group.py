69c47eaa8b32d8cb9855aade0356fa31c0c7e135
alzealze <Zemskovjob4@gmail.com>
2023-09-24 17:28:55 +0300

    Почистил код: убрал лишние методы и импорты

--- test_add_group.py
+++ test_add_group.py
@@ -1,19 +1,14 @@
 # -*- coding: utf-8 -*-
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
-import unittest, time, re
+import unittest
+
 
 class TestAddGroup(unittest.TestCase):
     def setUp(self):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
 
-    
     def test_add_group(self):
         wd = self.wd
         wd.get("http://localhost/addressbook/")
@@ -40,19 +35,10 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element(By.NAME, "submit").click()
         wd.find_element(By.LINK_TEXT, "group page").click()
         wd.find_element(By.LINK_TEXT, "Logout").click()
-    
-    def is_element_present(self, how, what):
-        try: self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
-    
-    def is_alert_present(self):
-        try: self.wd.switch_to_alert()
-        except NoAlertPresentException as e: return False
-        return True
 
     def tearDown(self):
         self.wd.quit()
+
 
 if __name__ == "__main__":
     unittest.main()
