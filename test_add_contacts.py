d1ad549b8aff2fe7605d57e07ccdec80baafba08
alzealze <Zemskovjob4@gmail.com>
2023-09-24 23:20:51 +0300

    Передал параметры в методы

--- test_add_contacts.py
+++ test_add_contacts.py
@@ -11,39 +11,61 @@ class TestAddContacts(unittest.TestCase):
 
     def test_add_contacts(self):
         wd = self.wd
-        # Открываем домашнюю страницу
-        wd.get("http://localhost/addressbook")
-        # login
-        wd.find_element(By.NAME, "user").click()
-        wd.find_element(By.NAME, "user").click()
-        wd.find_element(By.NAME, "user").clear()
-        wd.find_element(By.NAME, "user").send_keys("admin")
-        wd.find_element(By.ID, "LoginForm").click()
-        wd.find_element(By.NAME, "pass").click()
-        wd.find_element(By.NAME, "pass").clear()
-        wd.find_element(By.NAME, "pass").send_keys("secret")
-        wd.find_element(By.XPATH, "//input[@value='Login']").click()
-        # Открываем создание контактов add new
-        wd.find_element(By.LINK_TEXT, "add new").click()
+        self.open_home_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.open_contact_page(wd)
+        self.create_contacts(wd, firstname="Aleksandr", lastname="Zemskov", mobile="89201234567", nickname="alze")
+        self.returt_home_page(wd)
+        self.logout(wd)
+
+    def test_add_contacts_other(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.open_contact_page(wd)
+        self.create_contacts(wd, firstname="Aleksandr1", lastname="Zemskov1", mobile="89876543210", nickname="alze2")
+        self.returt_home_page(wd)
+        self.logout(wd)
+
+    def logout(self, wd):
+        wd.find_element(By.LINK_TEXT, "Logout").click()
+
+    def returt_home_page(self, wd):
+        wd.find_element(By.LINK_TEXT, "home page").click()
+
+    def create_contacts(self, wd, firstname, lastname, mobile, nickname):
         # заполнение формы контакты
         wd.find_element(By.NAME, "firstname").click()
         wd.find_element(By.NAME, "firstname").clear()
-        wd.find_element(By.NAME, "firstname").send_keys("Aleksandr")
+        wd.find_element(By.NAME, "firstname").send_keys(firstname)
         wd.find_element(By.NAME, "lastname").click()
         wd.find_element(By.NAME, "lastname").clear()
-        wd.find_element(By.NAME, "lastname").send_keys("Zemskov")
+        wd.find_element(By.NAME, "lastname").send_keys(lastname)
         wd.find_element(By.NAME, "mobile").click()
         wd.find_element(By.NAME, "mobile").clear()
-        wd.find_element(By.NAME, "mobile").send_keys("89201234567")
+        wd.find_element(By.NAME, "mobile").send_keys(mobile)
         wd.find_element(By.NAME, "nickname").click()
         wd.find_element(By.NAME, "nickname").clear()
-        wd.find_element(By.NAME, "nickname").send_keys("alze")
+        wd.find_element(By.NAME, "nickname").send_keys(nickname)
         # Подтверждение заполнения формы контакты
         wd.find_element(By.XPATH, "//div[@id='content']/form/input[21]").click()
-        # Возврат на главную страницу
-        wd.find_element(By.LINK_TEXT, "home page").click()
-        # Logout
-        wd.find_element(By.LINK_TEXT, "Logout").click()
+
+    def open_contact_page(self, wd):
+        wd.find_element(By.LINK_TEXT, "add new").click()
+
+    def login(self, wd, username, password):
+        wd.find_element(By.NAME, "user").click()
+        wd.find_element(By.NAME, "user").click()
+        wd.find_element(By.NAME, "user").clear()
+        wd.find_element(By.NAME, "user").send_keys(username)
+        wd.find_element(By.ID, "LoginForm").click()
+        wd.find_element(By.NAME, "pass").click()
+        wd.find_element(By.NAME, "pass").clear()
+        wd.find_element(By.NAME, "pass").send_keys(password)
+        wd.find_element(By.XPATH, "//input[@value='Login']").click()
+
+    def open_home_page(self, wd):
+        wd.get("http://localhost/addressbook")
 
     def tearDown(self):
         self.wd.quit()
