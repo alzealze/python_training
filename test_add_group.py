612062bbc488782d223e19725d81d9f21de35473
alzealze <Zemskovjob4@gmail.com>
2023-09-26 23:56:27 +0300

    тесты для групп переведены на pytest c использованием фикстуры

--- test_add_group.py
+++ test_add_group.py
@@ -1,26 +1,24 @@
 # -*- coding: utf-8 -*-
 from application import Application
 from group import Group
-import unittest
+import pytest
 
 
-class TestAddGroup(unittest.TestCase):
-    def setUp(self):
-        self.app = Application()
-
-    def test_add_group(self):
-        self.app.login("admin", "secret")
-        self.app.create_group(Group("dfgdfg", "dfgdfg", "dfgdfgdfg"))
-        self.app.logout()
-
-    def test_add_empty_group(self):
-        self.app.login("admin", "secret")
-        self.app.create_group(Group("", "", ""))
-        self.app.logout()
-
-    def tearDown(self):
-        self.app.destroy()
+@pytest.fixture
+def app(request):
+    """Создаем, разрушаем, возвращаем фикстуру"""
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_add_group(app):
+    app.login("admin", "secret")
+    app.create_group(Group("dfgdfg", "dfgdfg", "dfgdfgdfg"))
+    app.logout()
+
+
+def test_add_empty_group(app):
+    app.login("admin", "secret")
+    app.create_group(Group("", "", ""))
+    app.logout()
