8b90c7855b863e32ffad433c9d6177f075c3d102
alzealze <Zemskovjob4@gmail.com>
2023-09-30 17:49:04 +0300

    добавлен тест создания пустого контакта test_add_empty_group

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -5,3 +5,9 @@ def test_add_contact(app):
     app.session.login(username="admin", password="secret")
     app.contact.create(Contact(firstname="Aleksandr", lastname="Zemskov", mobile="89201234567", nickname="alze"))
     app.session.logout()
+
+
+def test_add_empy_contact(app):
+    app.session.login(username="admin", password="secret")
+    app.contact.create(Contact(firstname="", lastname="", mobile="", nickname=""))
+    app.session.logout()
