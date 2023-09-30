ef57ecdc05c8d0d5f0a08ee2380cca1daa54f5bf
alzealze <Zemskovjob4@gmail.com>
2023-09-30 13:37:32 +0300

    создан файл для создания контакта, тест из группы перенесен в него. def appcont(request): -отсутствует в проекте. тесты работают

---
+++ test/test_add_contact.py
@@ -0,0 +1,7 @@
+from model.group import Contact
+
+
+def test_add_contact(app):
+    app.session.login(username="admin", password="secret")
+    app.contact.create(Contact(firstname="Aleksandr", lastname="Zemskov", mobile="89201234567", nickname="alze"))
+    app.session.logout()
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from model.group import Group, Contact
+from model.group import Group
 
 
 def test_add_group(app):
@@ -11,10 +11,4 @@ def test_add_group(app):
 def test_add_empty_group(app):
     app.session.login("admin", "secret")
     app.group.create(Group("", "", ""))
-    app.session.logout()
-
-
-def test_add_contact(app):
-    app.session.login(username="admin", password="secret")
-    app.contact.create(Contact(firstname="Aleksandr", lastname="Zemskov", mobile="89201234567", nickname="alze"))
     app.session.logout()
