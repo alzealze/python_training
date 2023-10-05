1ad559d9b2420015cf7a847438d84fc5a5555d69
alzealze <Zemskovjob4@gmail.com>
2023-10-05 22:01:39 +0300

    Добавлено предусловие в тест модификации контакта

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -2,8 +2,12 @@ from model.contact import Contact
 
 
 def test_modify_contact_firstname(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(firstname="test"))
     app.contact.modify_first_contact(Contact(firstname="New firstname"))
 
 
 def test_modify_contact_lastname(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(firstname="firstname_test", lastname="lastname_test"))
     app.contact.modify_first_contact(Contact(lastname="New lastname"))
