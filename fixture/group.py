5d3ef81068e945dd0585bcc3e7471f06113fb7e8
alzealze <Zemskovjob4@gmail.com>
2023-10-05 20:31:23 +0300

    Добавлено предусловие в тест удаления группы

--- fixture/group.py
+++ fixture/group.py
@@ -58,3 +58,8 @@ class GroupHelper:
         # подтверждение модификации группы
         wd.find_element(By.NAME, "update").click()
         self.return_to_groups_page()
+
+    def count(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        return len(wd.find_elements(By.NAME, "selected[]"))
