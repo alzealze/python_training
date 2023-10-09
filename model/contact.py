421e4307edd9151aeab99eb11a17c11b3b55b690
alzealze <Zemskovjob4@gmail.com>
2023-10-09 21:02:08 +0300

    Реализована проверка равенства объектов и списков в тесте удаления контакта

--- model/contact.py
+++ model/contact.py
@@ -6,3 +6,9 @@ class Contact:
         self.mobile = mobile
         self.nickname = nickname
         self.id = id
+
+    def __repr__(self):
+        return "%s:%s" % (self.id, self.firstname)
+
+    def __eq__(self, other):
+        return self.id == other.id and self.firstname == other.firstname
