d65005a1d2183e79ab3b77579d7db6ecaa867862
alzealze <Zemskovjob4@gmail.com>
2023-10-10 00:17:10 +0300

    Добавлена сортировка списков и их сравнение для групп

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -4,13 +4,19 @@ from model.group import Group
 
 def test_add_group(app):
     old_groups = app.group.get_group_list()
-    app.group.create(Group("Б40", "python", "python_comment"))
+    group = (Group("Б40", "python", "python_comment"))
+    app.group.create(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) + 1 == len(new_groups)
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
 def test_add_empty_group(app):
     old_groups = app.group.get_group_list()
-    app.group.create(Group("", "", ""))
+    group = (Group("Б40", "python", "python_comment"))
+    app.group.create(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) + 1 == len(new_groups)
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
