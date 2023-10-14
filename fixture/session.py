3e9abd79ef93521ce29dc1cbf334667295d6d0fb
alzealze <Zemskovjob4@gmail.com>
2023-10-14 16:31:26 +0300

    Добавлен метод get_logged_user , а также проверен код на наличие экранирования

--- fixture/session.py
+++ fixture/session.py
@@ -43,4 +43,9 @@ class SessionHelper:
 
     def is_logged_in_as(self, username):
         wd = self.app.wd
-        return wd.find_element(By.XPATH, "//div[@id='top']/form/b").text == "("+username+")"
+        return self.get_logged_user() == username
+
+    def get_logged_user(self):
+        wd = self.app.wd
+        # берем из браузера только username (без кавычек)
+        return wd.find_element(By.XPATH, "//div[@id='top']/form/b").text[1:-1]
