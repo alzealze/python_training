7ed372f4d13938ba5a44e3b594cfca34da784955
alzealze <Zemskovjob4@gmail.com>
2023-10-22 11:14:29 +0300

    Реализована загрузка информации о тестируемом приложении из конфигурационного файла. !!Но по прежнему при запуске из PyCharm требуется выставлять путь до директории проекта в run-edit_configuration поле working directory

--- conftest.py
+++ conftest.py
@@ -1,23 +1,28 @@
 from fixture.application import Application
 import pytest
+import json
+import os.path
+
 
 fixture = None
+target = None
 
 
 @pytest.fixture
 def app(request):
     """Создаем, разрушаем, возвращаем фикстуру"""
     global fixture
+    global target
     browser = request.config.getoption("--browser")
-    baseUrl = request.config.getoption("--baseUrl")
-    password = request.config.getoption("--password")
-    username = request.config.getoption("--username")
-    if fixture is None:
-        fixture = Application(browser=browser, baseUrl=baseUrl, username=username, password=password)
-    else:
-        if not fixture.is_valid():
-            fixture = Application(browser=browser, baseUrl=baseUrl, username=username, password=password)
-    fixture.session.ensure_login(username=username, password=password)
+    if target is None:
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption("--target"))
+        # print("config_fileconfig_fileconfig_fileconfig_file", config_file)
+        with open(config_file) as f:
+            target = json.load(f)
+    if fixture is None or not fixture.is_valid():
+        fixture = Application(browser=browser, baseUrl=target["baseUrl"], username=target["username"],
+                              password=target["password"])
+    fixture.session.ensure_login(username=target["username"], password=target["password"])
     return fixture
 
 
@@ -32,6 +37,4 @@ def stop(request):
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="firefox")
-    parser.addoption("--baseUrl", action="store", default="http://localhost/addressbook/")
-    parser.addoption("--username", action="store", default="admin")
-    parser.addoption("--password", action="store", default="secret")
+    parser.addoption("--target", action="store", default="target.json")
