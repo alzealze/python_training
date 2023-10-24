71e151a9443982dc26d065ee6cd4fa17248db813
alzealze <Zemskovjob4@gmail.com>
2023-10-24 18:36:52 +0300

    Реализованы генераторы данных для групп и контактов с помощью jsonpickle, сохраняющие созданные данные в файл в формате JSON.

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -2,9 +2,8 @@
 from model.group import Group
 
 
-# data_groups - означет, что будем загружать тестовые данные из модуля groups в пакете data
-def test_add_group(app, data_groups):
-    group = data_groups
+def test_add_group(app, json_groups):
+    group = json_groups
     old_groups = app.group.get_group_list()
     app.group.create(group)
     assert len(old_groups) + 1 == app.group.count()
