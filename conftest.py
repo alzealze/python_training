9a4bb23eb1a64b18a042f6fb91dd69c30a6f9de4
alzealze <Zemskovjob4@gmail.com>
2023-10-30 16:01:16 +0300

    Создана фикстура для БД

--- conftest.py
+++ conftest.py
@@ -4,27 +4,43 @@ import json
 import jsonpickle
 import os.path
 import importlib
+from fixture.db import DbFixure
 
 
 fixture = None
 target = None
 
 
+def load_config(file):
+    global target
+    if target is None:
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), file)
+        with open(config_file) as f:
+            target = json.load(f)
+    return target
+
+
 @pytest.fixture
 def app(request):
     """Создаем, разрушаем, возвращаем фикстуру"""
     global fixture
-    global target
     browser = request.config.getoption("--browser")
-    if target is None:
-        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption("--target"))
-        with open(config_file) as f:
-            target = json.load(f)
+    web_config = load_config(request.config.getoption("--target"))["web"]
     if fixture is None or not fixture.is_valid():
-        fixture = Application(browser=browser, baseUrl=target["baseUrl"], username=target["username"],
-                              password=target["password"])
-    fixture.session.ensure_login(username=target["username"], password=target["password"])
+        fixture = Application(browser=browser, baseUrl=web_config["baseUrl"], username=web_config["username"],
+                              password=web_config["password"])
+    fixture.session.ensure_login(username=web_config["username"], password=web_config["password"])
     return fixture
+
+
+@pytest.fixture(scope="session")
+def db(reuqest):
+    db_config = load_config(request.config.getoption("--target"))["db"]
+    dbfixture = DbFixure(host=db_config["host"], name=db_config["name"],user=db_config["user"],password=db_config["password"])
+    def fin():
+        dbfixture.destroy()
+    reuqest.addfinalizer(fin)
+    return dbfixture
 
 
 @pytest.fixture(scope="session", autouse=True)
@@ -58,4 +74,3 @@ def load_from_module(module):
 def load_from_json(file):
     with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "data/%s.json" % file)) as f:
         return jsonpickle.decode(f.read())
-
