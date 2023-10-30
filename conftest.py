7f3bde6efbc12338d6e364043309f4a29458636a
alzealze <Zemskovjob4@gmail.com>
2023-10-30 14:46:30 +0300

    установлены пакеты mysql и pymysql , создан файл check_db_connection для проверки соединения с БД

--- conftest.py
+++ conftest.py
@@ -41,7 +41,6 @@ def pytest_addoption(parser):
     parser.addoption("--target", action="store", default="target.json")
 
 
-#@pytest.hookimpl(optionalhook=True)
 def pytest_generate_tests(metafunc):
     for fixture in metafunc.fixturenames:
         if fixture.startswith("data_"):
