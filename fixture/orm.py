6d187201e43a680c3083a5767ed342c2358a8724
alzealze <Zemskovjob4@gmail.com>
2023-11-09 23:20:24 +0300

    Дополнена fixtue/orm.py (Отношения между объектами)

--- fixture/orm.py
+++ fixture/orm.py
@@ -4,6 +4,7 @@ from model.group import Group
 from model.contact import Contact
 # from pymysql.converters import decoders
 
+
 class ORMFixture:
 
     db = Database()
@@ -14,6 +15,7 @@ class ORMFixture:
         name = Optional(str, column="group_name")
         header = Optional(str, column="group_header")
         footer = Optional(str, column="group_footer")
+        contacts = Set(lambda: ORMFixture.ORMContact, table="address_in_groups", column="id", reverse="groups", lazy=True)
 
     class ORMContact(db.Entity):
         _table_ = "addressbook"
@@ -21,6 +23,7 @@ class ORMFixture:
         firstname = Optional(str, column="firstname")
         lastname = Optional(str, column="lastname")
         deprecated = Optional(datetime, column="deprecated")
+        groups = Set(lambda: ORMFixture.ORMGroup, table="address_in_groups", column="group_id", reverse="contacts", lazy=True)
 
     def __init__(self, host, name, user, password):
         self.db.bind('mysql', host=host, database=name, user=user, password=password)
@@ -44,3 +47,18 @@ class ORMFixture:
     @db_session
     def get_contact_list(self):
         return self.convert_contacts_to_model(select(c for c in ORMFixture.ORMContact if c.deprecated is None))
+
+    @db_session
+    def get_contact_in_group(self, group):
+        # Извлекаем группу с заданным id
+        orm_group = list(select(g for g in ORMFixture.ORMGroup if g.id == group.id))[0]
+        # Преобразуем объекты типа ORM contacts в модельные объекты
+        return self.convert_contacts_to_model(orm_group.contacts)
+
+    @db_session
+    def get_contact_not_in_group(self, group):
+        orm_group = list(select(g for g in ORMFixture.ORMGroup if g.id == group.id))[0]
+        # Выбираем все контакты, у которых список групп не содержит заданную группу(если контакт с этой группой связан,
+        # то он в эту выборку не попадет)
+        return self.convert_contacts_to_model(
+            select(c for c in ORMFixture.ORMContact if c.deprecated is None and orm_group not in c.groups))
